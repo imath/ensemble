4d626c108d173e07f844e80a2ff93f5084510a68
imath <math.viet@gmail.com>
2022-12-28 14:09:11 +0100

    Enqueue theme style & improve header and footer layout

--- functions.php
+++ functions.php
@@ -2,9 +2,11 @@
 /**
  * Ensemble functions and definitions
  *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
  * @package /ensemble
  *
- * @since  1.0.0
+ * @since 1.0.0
  */
 
 if ( ! function_exists( 'ensemble_setup' ) ) {
@@ -18,3 +20,18 @@ if ( ! function_exists( 'ensemble_setup' ) ) {
 	}
 }
 add_action( 'after_setup_theme', 'ensemble_setup' );
+
+/**
+ * Enqueue the style.css file.
+ *
+ * @since 1.0.0
+ */
+function ensemble_enqueue_styles() {
+	wp_enqueue_style(
+		'ensemble',
+		get_stylesheet_uri(),
+		array(),
+		wp_get_theme()->get( 'Version' )
+	);
+}
+add_action( 'wp_enqueue_scripts', 'ensemble_enqueue_styles' );
