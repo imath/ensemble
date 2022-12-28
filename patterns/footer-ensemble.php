4814d76a4aeeb7f16195c3ec63a26b2c5d8529d3
imath <math.viet@gmail.com>
2022-12-28 13:18:44 +0100

    Use a pattern to display the site’s footer

---
+++ patterns/footer-ensemble.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Title: Footer Ensemble
+ * Slug: ensemble/footer-ensemble
+ * Categories: footer
+ * Block Types: core/template-part/footer
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"layout":{"inherit":true}} -->
+<div class="wp-block-group">
+	<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
+	<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--40)">
+		<!-- wp:social-links -->
+		<ul class="wp-block-social-links">
+		<?php
+		printf(
+			__( '<!-- wp:social-link {"url":"%s","service":"feed"} /-->' ),
+			esc_url( get_bloginfo( 'rss2_url' ) )
+		);
+		?>
+		<!-- wp:social-link {"url":"https://twitter.com/imath","service":"twitter"} /-->
+		<!-- wp:social-link {"url":"https://github.com/imath","service":"github"} /-->
+		</ul>
+		<!-- /wp:social-links -->
+		<p class="has-text-align-right">
+		<?php
+		printf(
+			/* Translators: WordPress link. */
+			esc_html__( 'Fièrement propulsé par %s', 'ensemble' ),
+			'<a href="' . esc_url( __( 'https://fr.wordpress.org/', 'ensemble' ) ) . '" rel="nofollow">WordPress</a>'
+		);
+		?>
+		<span class="sep"> | </span>
+		<?php
+		printf(
+			/* translators: 1: Theme name, 2: Theme author. */
+			esc_html__( 'Thème: %1$s de %2$s.', 'ensemble' ),
+			'<i><a href="' . esc_url( 'https://github.com/imath/ensemble' ) .'">Ensemble</a></i>',
+			'<a href="' . esc_url( 'https://imathi.eu/' ) .'">imath</a>'
+		);
+
+		if ( function_exists( 'the_privacy_policy_link' ) ) {
+			the_privacy_policy_link( '<span class="sep"> | </span>', '' );
+		}
+		?>
+		</p>
+		<!-- /wp:paragraph -->
+	</div>
+	<!-- /wp:group -->
+</div>
+<!-- /wp:group -->
