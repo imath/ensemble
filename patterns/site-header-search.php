34b4796f82c89d6e5f0cbffab6a0eaf5efe0595f
imath <math.viet@gmail.com>
2023-01-01 20:19:04 +0100

    Add a search form into the site header

---
+++ patterns/site-header-search.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Title: Site Header search
+ * Slug: ensemble/site-header-search
+ * Categories: header
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"layout":{"inherit":true},"className":"search-form-container"} -->
+<div class="wp-block-group search-form-container">
+	<!-- wp:search {"buttonPosition":"button-inside","buttonUseIcon":true,"showLabel":false,"placeholder":"<?php echo esc_attr_x( 'Texte recherché...', 'placeholder for search field', 'ensemble' ); ?>"} /-->
+</div>
+<!-- /wp:group -->
