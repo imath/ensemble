6ed3ae53ef536ad9eaed78d57d5bf56f599e25e1
imath <math.viet@gmail.com>
2023-08-21 23:00:49 +0200

    Improve signet layout and add a footer

--- patterns/post-formats.php
+++ patterns/post-formats.php
@@ -36,12 +36,27 @@
 <!-- /wp:ensemble/post-format-template -->
 
 <!-- wp:ensemble/post-format-template {"format":"link"} -->
-	<!-- wp:group {"tagName":"section","className":"entry-excerpt","layout":{"inherit":true}} -->
-	<section class="wp-block-group entry-excerpt">
+	<!-- wp:group {"tagName":"section","className":"entry-excerpt signet-content","layout":{"inherit":true}} -->
+	<section class="wp-block-group entry-excerpt signet-content">
 		<!-- wp:spacer {"height":"10px"} -->
 		<div style="height:10px" aria-hidden="true" class="wp-block-spacer"></div>
 		<!-- /wp:spacer -->
 		<!-- wp:post-content /-->
 	</section>
 	<!-- /wp:group -->
+	<!-- wp:group {"tagName":"section","className":"entry-footer signet-footer","layout":{"inherit":true}} -->
+	<section class="wp-block-group entry-footer signet-footer">
+		<!-- wp:group {"layout":{"type":"flex"}} -->
+		<div class="wp-block-group">
+			<!-- wp:paragraph -->
+			<p><?php echo esc_html_x( 'Publié le', '"Published on" date separator', 'ensemble' ); ?></p>
+			<!-- /wp:paragraph -->
+			<!-- wp:post-date /-->
+		</div>
+		<!-- /wp:group -->
+	</section>
+	<!-- /wp:group -->
+	<!-- wp:spacer {"height":"20px"} -->
+	<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+	<!-- /wp:spacer -->
 <!-- /wp:ensemble/post-format-template -->
