b40cfd02e0178fefb617916a29adcfed154c5194
imath <math.viet@gmail.com>
2023-09-23 13:52:17 +0200

    Remove signet/breve from h3 tags content

--- patterns/signet-header-three.php
+++ patterns/signet-header-three.php
@@ -12,7 +12,7 @@
 <!-- wp:group {"layout":{"type":"flex"}} -->
 <div class="wp-block-group">
 	<!-- wp:heading {"level":3,"className":"wp-block-post-title"} -->
-		<h3 class="wp-block-post-title"><?php echo esc_html_x( 'Signet publié le', '"Published on" date separator', 'ensemble' ); ?></h3>
+		<h3 class="wp-block-post-title"><?php echo esc_html_x( 'Publié le', '"Published on" date separator', 'ensemble' ); ?></h3>
 	<!-- /wp:heading -->
 	<!-- wp:post-date {"fontSize":"grande","textColor":"black"} /-->
 </div>
